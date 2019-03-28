fdca2ecdd5e4e48a22e59816909395c2ed06e205
Justin <justin@baal-software.com>
2019-03-28 10:24:36 -0600

    Complete Challenge 2

--- main.js
+++ main.js
@@ -5,11 +5,20 @@ var employees = [
         {first: "Amanda", last: "Byron", group: "Sales"},
         {first: "Ye", last: "Xia", group: "Receiving", nameOrder: "reverse"},
         {first: "Miltiades", last: "Crescens", group: "Sales"},
-        /*...don't foget to account for other entries of the same form, but with different group names.....*/
+        /*...don't forget to account for other entries of the same form, but with different group names.....*/
     ];
 
 // Part 2 Answer Here
+const groupEmployees = employees => (
+    employees.reduce((acc, employee)=>{
 
+      if(!acc[employee.group]){
+        acc[employee.group] = []
+      }
+        acc[employee.group].push({name: `${employee.first} ${employee.last}`})
+      return acc
+    },{})
+  )
 /*****************************************************************************************
 * Bonus
 ****************************************************************************************/
